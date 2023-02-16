8fe3c10f982e5b5e2df55966817d601187784079
Ryan Veitch <ryan@veitch.cc>
2023-02-16 13:53:20 +0000

    Updating to free text input

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ const apiKey = urlParams.get('apiKey');
 
 function submit() {
 
-let firstName = document.getElementById('name').value;
-let pizza = document.getElementById('pizza').value;
-let drink = document.getElementById('drink').value;
+let orderDetails = document.getElementById('orderPreference').value;
 
-console.log(firstName + '\n' + pizza + '\n' + drink)
 
   var data = JSON.stringify([
     {
       "event": "NRUG",
       "src": "webForm",
-      "message": `NEW ORDER SUBMITTED. name=${firstName} pizza=${pizza} and drink=${drink}`
+      "message": `NEW ORDER SUBMITTED. ${orderDetails}`
     }
   ]);
 
@@ -32,5 +29,43 @@ console.log(firstName + '\n' + pizza + '\n' + drink)
 
   
   xhr.send(data);
-
 }
+
+function sendText() {
+
+  let freeText = document.getElementById('textInput').value;
+  
+  
+  //console.log(firstName + '\n' + pizza + '\n' + drink)
+  
+    var data = JSON.stringify([
+      {
+        "event": "NRUG",
+        "src": "webForm",
+        "message": `${freeText}`
+      }
+    ]);
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("POST", `https://log-api.newrelic.com/log/v1?Api-Key=${apiKey}`);
+    xhr.setRequestHeader("Content-Type", "application/json");
+  
+    
+    xhr.send(data);
+  
+  }
+
+
+  function nextPage() {
+
+    location.href = `./food.html?apiKey=${apiKey}`
+
+  }
