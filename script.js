a0a6e1d5f4f80b802bff9948c45516b8f8185daf
Ryan Veitch <ryan@veitch.cc>
2023-03-10 10:29:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ let orderDetails = document.getElementById('orderPreference').value;
   xhr.open("POST", `https://log-api.newrelic.com/log/v1?Api-Key=${apiKey}`);
   
   xhr.send(data);
+
+  var getValue= document.getElementById("orderPreference");
+  if (getValue.value !="") {
+      getValue.value = "";
+  }
+
 }
 
 function sendText() {
@@ -56,6 +62,11 @@ function sendText() {
     xhr.open("POST", `https://log-api.newrelic.com/log/v1?Api-Key=${apiKey}`);  
     
     xhr.send(data);
+
+    var getValue= document.getElementById("textInput");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }
   
   }
 
